2a586eea391ebfdb5c245f86ac08a3d4f752aeaa
Joseph <30614712+Joseph@users.noreply.github.com>
2019-11-05 16:09:30 -0500

    Update grapher.py

--- grapher.py
+++ grapher.py
@@ -10,6 +10,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.pylab import zeros, append
 from math import sin
+
+import matplotlib.animation as animation
+
+
 figure_1 = figure(num=0, figsize=  (12, 8))
 figure_1.suptitle("SinFunction and Derivatives", fontsize=12)
 
@@ -25,9 +29,9 @@ axis_2.set_title("sin'(x)")
 axis_3.set_title("sin''(x)")
 
 # Change graph heights
-axis_1.set_ylim((0, 2))
-axis_2.set_ylim((0, 2))
-axis_3.set_ylim((0, 2))
+axis_1.set_ylim((-1, 1))
+axis_2.set_ylim((-1, 1))
+axis_3.set_ylim((-1, 1))
 
 # sex x-limits
 axis_1.set_xlim(0,5.0)
@@ -64,7 +68,7 @@ x = zeros(0) # We also need x values to be zeros too. This finished our placehol
 # With titles nad line colors
 sin_x_plot, = axis_1.plot(x, axis_1_data, 'b-',label="sin(x)")
 sin_x_d1_plot, = axis_2.plot(x, axis_2_data, 'b-', label="sin'(x)")
-sin_x_d2_plot, = axis_3.plot(x, axis_3_data, 'b-', label="sin'(x)")
+sin_x_d2_plot, = axis_3.plot(x, axis_3_data, 'b-', label="sin''(x)")
 
 
 # Then we add a legend. This consists of adding the lines we plotted associated with their names
@@ -73,19 +77,42 @@ axis_1.legend([sin_x_plot], [sin_x_plot.get_label()])
 axis_2.legend([sin_x_d1_plot], [sin_x_d1_plot.get_label()])
 axis_3.legend([sin_x_d2_plot], [sin_x_d2_plot.get_label()])
 
-def updateFunction(x):
-    global x, axis_1_data, axis_2_data, axis_3_data, axis_3_data
-    
-    y = lambda x: sin(x)
-    dy_dx = lambda x: cos(x) 
-    dy_dx_dx = lambda x: -sin(x)
-    
-    axis_1_data = append(axis_1_data, )
-    
-    
+x_t = 0 # starting theta = 0
+max_t = 5
 
+def updateFunction(self):
+    global x_t, axis_1_data, axis_2_data, axis_3_data, axis_4_data, x
+    
+    #y = lambda x: sin(x)
+    #dy_dx = lambda x: cos(x) 
+    #y_dx_dx = lambda x: -sin(x)
+    y = sin(x_t)# a list of points: y, let b be iter(y); next(b)
+    dy_dx = cos(x_t) # like for a list of points: y, let b be iter(y); next(b)
+    y_dx_dx = -sin(x_t) # like for a list of points: y, let b be iter(y); next(b)
+    
+    axis_1_data = append(axis_1_data, y)
+    axis_2_data = append(axis_2_data, dy_dx)
+    axis_3_data = append(axis_3_data, y_dx_dx)
+    x = append(x, x_t)
+    
+    
+    sin_x_plot.set_data(x, axis_1_data)
+    sin_x_d1_plot.set_data(x, axis_2_data)
+    sin_x_d2_plot.set_data(x, axis_3_data)
+    
+    x_t += 0.05
+    
+    if x_t >= max_t - 1.00:        
+        sin_x_plot.axes.set_xlim(x_t-max_t+1.0,x_t+1.0)
+        sin_x_d1_plot.axes.set_xlim(x_t-max_t+1.0,x_t+1.0)
+        sin_x_d2_plot.axes.set_xlim(x_t-max_t+1.0,x_t+1.0)
+        
+        
+    return sin_x_plot, sin_x_d1_plot, sin_x_d2_plot
+    
+simulation = animation.FuncAnimation(figure_1, updateFunction, blit=False, frames=200, interval=100, repeat=True)
 
-
+matplotlib.pylab.plt.show()
 # To recap:
 #   We: 
 #       Created our figure and 3 axis sets, one for each graph we want to show up
@@ -95,8 +122,5 @@ def updateFunction(x):
 #       Added placeholder data to our graph,
 #       Created line plots associated with each dataset for our plots. These are automatically added to our axis so they show up
 #       Made a legend with the respective line plots we want to show on each graph
-
-frameWidth = 20
-y = lambda x: sin(x)
 
         

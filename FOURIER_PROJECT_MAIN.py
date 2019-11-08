b0be7955512d2a226768365eb6e87b5c088661e5
Ana <57271956+anaverul@users.noreply.github.com>
2019-11-08 17:41:29 -0500

    Update FOURIER_PROJECT_MAIN.py
    -The work of joseph Islam, posted here b/c of network troubles

--- FOURIER_PROJECT_MAIN.py
+++ FOURIER_PROJECT_MAIN.py
@@ -26,7 +26,7 @@ import matplotlib.animation as animation
 import matplotlib
 
 # Make the figure framework to hold the Transform and wav file
-figure_1 = figure(num=0, figsize = (12, 8))
+figure_1 = figure(num=0, figsize = (12, 9))
 figure_1.suptitle("Wave Fourier Transform Analysis", fontsize=12)
 
 # Make the axis
@@ -39,16 +39,51 @@ axis_2.set_title("Notes comprising sound in Song waveform window")
 
 # axis_1 rendering constants
 WAVEFORM_WINDOW_WIDTH = 1 # How many seconds in width to render (s)
-WAVEFORM_MAX_AMPLITUDE = 100 # Idk what this value is. Anna -- change it to what makes sense?
-WAVEFORM_MIN_AMPLITUDE = 10  # likewise...
+MAX_AMPLITUDE = 100 # Idk what this value is. Anna -- change it to what makes sense?
+MIN_AMPLITUDE = 10  # likewise...
 
 
 # axis_2 rendering constants
 FREQUENCY_MAX = 200000  # max range of human hearing
 FREQUENCY_MIN = 20      # min range of human hearing
 
-axis_1.set_xlim(WAVEFORM_WINDOW_WIDTH)  # Set the viewing window size
+# Set x limits for waveform and frequency domain
+axis_1.set_xlim((0, WAVEFORM_WINDOW_WIDTH))  # Set the viewing window size
 axis_2.set_xlim((FREQUENCY_MIN, FREQUENCY_MAX)) # Frequency range for our analysis
 
-axis_1.set_ylim((FREQUENCY_MIN, FREQUENCY_MAX))
-axis_2.set_ylim((-FREQUENCY_MAX, FREQUENCY_MAX))
+# Set y limits for waveform and frequency amplitude
+axis_1.set_ylim((MIN_AMPLITUDE, MAX_AMPLITUDE))
+axis_2.set_ylim((-MAX_AMPLITUDE, MAX_AMPLITUDE))
+
+# Add grids to the axis
+axis_1.grid(True)
+axis_2.grid(True)
+
+# Add labels to x axis of both axis_1 and axis_2
+axis_1.set_xlabel("Time (s)")
+axis_2.set_xlabel("Frequency (radian/sec)")
+
+# Add labels to y axis of both axis_1 and axis_2
+axis_1.set_ylabel("Amplitude")
+axis_2.set_ylabel("Amplitude")
+
+# Make data placeholders 
+axis_1_data = zeros(0)
+axis_2_data = zeros(FREQUENCY_MAX)
+
+# Make data placeholders
+axis_1_x = zeros(0)
+axis_2_x = zeros(FREQUENCY_MAX)
+
+wav_plot, = axis_1.plot(axis_1_x, axis_1_data, '-b', label='wave')
+amp_freq, = axis_2.plot(axis_2_x, axis_2_data, '-b', label='Freqs')
+
+axis_1.legend([wav_plot], [wav_plot.get_label()])
+axis_2.legend([amp_freq], [amp_freq.get_label()])
+
+def updateFunction(self):
+    pass
+    
+
+simulation = animation.FuncAnimation(figure_1, updateFunction, blit=False, frames=200, interval=100, repeat=True)
+matplotlib.pylab.plt.show()
